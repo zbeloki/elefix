578fbf4cab9b98bde3948186323a5179384be52e
Zuhaitz Beloki <zbeloki@gmail.com>
2020-11-01 07:38:58 +0100

    Evaluation script implemented

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -4,88 +4,96 @@ import argparse
 import os
 import sys
 
-import plotly.express as px
+DEFAULT_VERT_THRESHOLD = 5.0
 
+def main(dataset_dir):
 
-def main(track_fpath):
+    # validate input directory
+    if not os.path.isdir(dataset_dir):
+        raise ValueError('"dataset_dir" is not a valid directory path')
 
-    # validate input
-    fname, ext = os.path.splitext(track_fpath)
-    if ext.lower() not in ['.gpx', '.tcx']:
-        raise ValueError("Input file's format must be GPX or TCX")
+    # prepare dataset
+    # each dataset entry: {'lats', 'lons', 'dists', 'alts_orig', 'alts_srtm_raw'}
+    dataset = []
+    for fname in os.listdir(dataset_dir):
+        
+        # validate file
+        _, ext = os.path.splitext(fname)
+        fpath = os.path.join(dataset_dir, fname)
+        if not os.path.isfile(fpath) or ext not in ['.gpx', '.tcx']:
+            continue
 
-    # parse input file
-    with open(track_fpath, 'r') as f:
-        track_content = f.read()
-        if ext.lower() == '.gpx':
-            wpts = elevfix.gpx_parse(track_content)
-        else:  # ext.lower() == '.tcx'
-            wpts = elevfix.tcx_parse(track_content)
+        # parse input file
+        with open(fpath, 'r') as f:
+            track_content = f.read()
+            if ext.lower() == '.gpx':
+                wpts = elevfix.gpx_parse(track_content)
+            else:  # ext.lower() == '.tcx'
+                wpts = elevfix.tcx_parse(track_content)
 
-    # remove waypoints for which the distance to the next one is 0
-    i = 0
-    while i < len(wpts)-1:
-        if elevfix.wpt_distance(wpts[i], wpts[i+1]) == 0.0:
-            del wpts[i]
-        else:
-            i += 1
+        # remove waypoints for which the distance to the next one is 0
+        i = 0
+        while i < len(wpts)-1:
+            if elevfix.wpt_distance(wpts[i], wpts[i+1]) == 0.0:
+                del wpts[i]
+            else:
+                i += 1
 
-    # parsed original latitudes, longitudes and altitudes
-    latitudes = [ wpt.lat for wpt in wpts ]
-    longitudes = [ wpt.lon for wpt in wpts ]
-    altitudes_orig = [ wpt.alt for wpt in wpts ]
+        # original latitudes, longitudes and altitudes
+        latitudes = [ wpt.lat for wpt in wpts ]
+        longitudes = [ wpt.lon for wpt in wpts ]
+        altitudes = [ wpt.alt for wpt in wpts ]
 
-    # accumulated distance on each waypoint
-    dists = [ elevfix.wpt_distance(wpair[0], wpair[1]) for wpair in zip(wpts[:-1], wpts[1:]) ]
-    dists_acc = [ sum(dists[:i]) for i in range(len(wpts)) ]
+        # accumulated distance on each waypoint
+        dists = [ elevfix.wpt_distance(wpair[0], wpair[1]) for wpair in zip(wpts[:-1], wpts[1:]) ]
+        dists_acc = [ sum(dists[:i]) for i in range(len(wpts)) ]
 
-    ## calculate metrics for evaluation
+        # SRTM raw altitudes
+        alts_srtm_raw = elevfix.set_altitudes(latitudes, longitudes, smooth=False)
 
-    # original accumulated elevation gain
-    eg_orig = elevation_gain_with_threshold(altitudes_orig)
-    print('Original elevation gain: {:.2f}'.format(eg_orig))
+        dataset_entry = {
+            'lats': latitudes,
+            'lons': longitudes,
+            'dists': dists_acc,
+            'alts_orig': altitudes,
+            'alts_srtm_raw': alts_srtm_raw,
+        }
+        dataset.append(dataset_entry)
 
-    # SRTM raw altitudes
-    altitudes_srtm_raw = elevfix.set_altitudes(latitudes, longitudes, smooth=False)
-    o2_srtm = variance(altitudes_orig, altitudes_srtm_raw)
-    o_srtm = math.sqrt(o2_srtm)
-    eg_srtm = elevation_gain(altitudes_srtm_raw)
-    eg_th_srtm = elevation_gain_with_threshold(altitudes_srtm_raw)
-    print('SRTM raw:')
-    print('- Variance: {:.2f}'.format(o2_srtm))
-    print('- Standard deviation: {:.2f}'.format(o_srtm))
-    print('- Accumulated elevation gain: {:.2f}'.format(eg_srtm))
-    print('- Elevation gain with threshold: {:.2f}'.format(eg_th_srtm))
-
+    # SRTM raw altitudes (baseline)
+    sqr_avg_dev, avg_dev = calculate_deviation(dataset, None, None, None)
+    print('-\t{:.2f}\t{:.2f}'.format(sqr_avg_dev, avg_dev))
+    
     # SRTM smoothed altitudes after applying Savitzky-Golay filter
-    min_o = sys.maxsize
-    min_eg_diff = sys.maxsize
-    best_altitudes = []
-    best_window = None
-    for window in range(51, 501, 50):
-        
-        altitudes_srtm = elevfix.set_altitudes(latitudes, longitudes, smooth=True, window=window)
-        o2_srtm = variance(altitudes_orig, altitudes_srtm)
-        o_srtm = math.sqrt(o2_srtm)
-        eg_srtm = elevation_gain(altitudes_srtm)
-        eg_th_srtm = elevation_gain_with_threshold(altitudes_srtm)
-        print('SRTM smoothed (w = {}):'.format(window))
-        print('- Variance: {:.2f}'.format(o2_srtm))
-        print('- Standard deviation: {:.2f}'.format(o_srtm))
-        print('- Elevation gain: {:.2f}'.format(eg_srtm))
-        print('- Elevation gain with threshold: {:.2f}'.format(eg_th_srtm))
-        
-        if abs(eg_th_srtm - eg_orig) < min_eg_diff:
-            best_altitudes = altitudes_srtm
-            best_window = window
-            min_eg_diff = abs(eg_th_srtm - eg_orig)
-            min_o = o_srtm
+    best = [None, None, 999999, None]  # window, square_avg_dev, avg_dev
+    for window in range(11, 402, 10):
+        for polynom in [2, 3]:
+            sqr_avg_dev, avg_dev = calculate_deviation(dataset, window, DEFAULT_VERT_THRESHOLD, polynom)
+            print('{}\t{}\t{:.2f}\t{:.2f}'.format(window, polynom, sqr_avg_dev, avg_dev))
 
-    # draw diagram
-    wpts = [ {'dist':w[0], 'orig':w[1], 'srtm':w[2], 'srtm_smth':w[3]} for w in zip(dists_acc, altitudes_orig, altitudes_srtm_raw, best_altitudes) ]
-    fig = px.line(wpts, x='dist', y=['orig', 'srtm', 'srtm_smth'], title='Elevation profile (w={})'.format(best_window))
-    fig.show()
+            if sqr_avg_dev < best[2]:
+                best = [window, polynom, sqr_avg_dev, avg_dev]
 
+    print(best)
+
+
+def calculate_deviation(dataset, savgol_window, vertical_threshold = DEFAULT_VERT_THRESHOLD, polynom = 2):
+
+    sqr_deviation = 0.0
+    deviation = 0.0
+    for entry in dataset:
+        eg_orig = elevation_gain(entry['alts_orig'], DEFAULT_VERT_THRESHOLD)
+        if savgol_window is None and vertical_threshold is None:
+            eg_srtm = elevation_gain(entry['alts_srtm_raw'], DEFAULT_VERT_THRESHOLD)
+        else:
+            alts_srtm = elevfix.set_altitudes(entry['lats'], entry['lons'], True, savgol_window, polynom)
+            eg_srtm = elevation_gain(alts_srtm, vertical_threshold)
+        current_deviation = abs(eg_srtm - eg_orig)
+        sqr_deviation += (current_deviation ** 2)
+        deviation += current_deviation
+        
+    return ( math.sqrt(sqr_deviation / len(dataset)), (deviation / len(dataset)) ) 
+    
 
 def variance(altitudes1, altitudes2):
 
@@ -99,25 +107,13 @@ def variance(altitudes1, altitudes2):
     return sqr_sum / len(altitudes1)
 
 
-def elevation_gain(altitudes):
-
-    eg = 0.0
-    for wpt1_alt, wpt2_alt in zip(altitudes[:-1], altitudes[1:]):
-        ediff = wpt2_alt - wpt1_alt
-        if ediff > 0.0:
-            eg += ediff
-
-    return eg
-
-
-EG_THRESHOLD = 5.0
-def elevation_gain_with_threshold(altitudes):
+def elevation_gain(altitudes, threshold):
     
     eg = 0.0
     last_valid_i = 0
     for i in range(1, len(altitudes)):
         ediff = altitudes[i] - altitudes[last_valid_i]
-        if abs(ediff) >= EG_THRESHOLD:
+        if abs(ediff) >= threshold:
             last_valid_i = i
             if ediff > 0.0:
                 eg += ediff
@@ -128,7 +124,7 @@ def elevation_gain_with_threshold(altitudes):
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description="Calculates some metrics to evaluate inferred SRTM data")
-    parser.add_argument("track_file", help='Input track file (GPX or TCX)')
+    parser.add_argument("dataset_dir", help='Input directory containing the track dataset (GPX or TCX files)')
     args = parser.parse_args()
 
-    main(args.track_file)
+    main(args.dataset_dir)
