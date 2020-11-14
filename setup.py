669badbf6afd757f770cd7a6d167dea4c4544651
Zuhaitz Beloki <zbeloki@gmail.com>
2020-11-14 14:51:29 +0100

    renamed elevfix->elefix

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
 setuptools.setup(
-    name="elevfix",
+    name="elefix",
     version="0.0.1",
     author="Zuhaitz Beloki",
     author_email="zbeloki@gmail.com",
     description="",
     long_description="",
     long_description_content_type="text/markdown",
-    url="https://github.com/zbeloki/elevfix",
+    url="https://github.com/zbeloki/elefix",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
